565f9ba02912012da63d09d158409ba751ad18bb
JanKolo04 <jankolodziej04@gmail.com>
2022-08-15 22:19:06 +0200

    login system

--- index.php
+++ index.php
@@ -3,132 +3,81 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/style-index.css">
-	<title>Egzaminy na prawo jazdy</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<title>Zaloguj się - egzaminy na prawo jazdy</title>
 </head>
 <body>
 
-	<div id="main">
-	<?php
-	
-		include("connection.php");
 
-		$pytanieID = $_GET['pytanie'];
-		$zakres_struktury = $_GET['zakres_struktury'];
-
-		if($zakres_struktury == "podstawowy") {
-			get_data_podstawowe();
-		}
-		else {
-			get_data_specjalistyczne();
-		}
-
-		function get_data_podstawowe() {
-			global $con, $pytanieID;
-
-			//get data from table pytania
-			$sql = "SELECT * FROM pytania_podstawowe WHERE Id=$pytanieID";
-			$query = mysqli_query($con, $sql);
-
-			$data_array = mysqli_fetch_array($query);
-			$next = ($data_array['Id'])+1;
-
-			echo
-				"	
-					<div id='questionAndAnswer'>				
-						<p>".$data_array['Pytanie']."</p></br>
-						<button class='answer'>Tak</button>
-						<button class='answer'>Nie</button>
-					</div>
-				";
-
-			if($data_array['Id'] > 1) {
-				echo "<a class='btn btn-danger' href=index.php?pytanie=$previous&zakres_struktury=specjalistyczny>Poprzednie</a> ";
-			}
-
-			echo "<a class='btn btn-success' href=index.php?pytanie=$next&zakres_struktury=podstawowy>Następne</a>";
+	<div>
+		<form method="POST">
+			<input type="text" name="login" placeholder="Login...">
+			<input type="password" name="passwd" placeholder="Hasło...">
 			
-		}
+			<br><button type="submit" name="loginSubmit">Submit</button>
 
-		function get_data_specjalistyczne() {
-			global $con, $pytanieID;
-
-			//get data from table pytania
-			$sql = "SELECT * FROM pytania_specjalistyczne WHERE Id=$pytanieID";
-			$query = mysqli_query($con, $sql);
-
-			$data_array = mysqli_fetch_array($query);
-			$next = ($data_array['Id'])+1;
-			$previous = ($data_array['Id']) - 1;
-
-			echo
-				"	
-					<div id='questionAndAnswer'>				
-						<p>".$data_array['Pytanie']."</p></br>
-						<button class='answer'>".$data_array['Odpowiedz_A']."</button>
-						<button class='answer'>".$data_array['Odpowiedz_B']."</button>
-						<button class='answer'>".$data_array['Odpowiedz_C']."</button>
-					</div>
-				";
-
-			if($data_array['Id'] > 1) {
-				echo "<a class='btn btn-danger' href=index.php?pytanie=$previous&zakres_struktury=specjalistyczny>Poprzednie</a> ";
-			}
-
-			echo "<a class='btn btn-success' href=index.php?pytanie=$next&zakres_struktury=specjalistyczny>Następne</a>";
-			
-		}
-
-
-	?>
-
-		<div id="holderForA">
-			<div id="navigationButtons">
-				<a class="btn btn-primary" id="podstawowy" href=index.php?pytanie=1&zakres_struktury=podstawowy>Podstawowy</a>
-				<a class="btn btn-primary" id="specjalistyczny" href=index.php?pytanie=1&zakres_struktury=specjalistyczny>Specjalistyczny</a>
-			</div>
-		</div>
+			<br><label for="cookie">Zapamiętaj mnie</label>
+			<input id="cookie" type="checkbox" name="cookie">
+		</form>
 	</div>
 
 
+	<?php
+
+		include("connection.php");
+
+		if(isset($_COOKIE['login'])) {
+			header("Location: nauka.php?pytanie=1&zakres_struktury=podstawowy");
+		}
+
+		if(isset($_POST['loginSubmit'])) {
+			check_data();
+		}
+
+		if(isset($_POST['cookie'])) {
+			//set login cookie
+			setcookie("login", "$login", time() + (86400 * 30), "/"); // 86400 = 1 day
+		}
+
+		function get_data() {
+			global $login;
+			//get login
+			$login = $_POST['login'];
+			//password
+			$passwd = $_POST['passwd'];
+
+			$data = [
+				"Login"=>$login,
+				"Passwd"=>$passwd
+			];
+
+			return $data;
+		}
+
+		function check_data() {
+			global $con;
+
+			$user_data = get_data();
 
 
-	<script type="text/javascript">
-		
-		function change_button_color() {
-			let zakres = <?php echo json_encode($zakres_struktury); ?>;
-			console.log(zakres);
-			if(zakres == "podstawowy") {
-				document.querySelector("#podstawowy").style="background-color: grey !important;";
+			$sql = "SELECT * FROM users WHERE Login='{$user_data['Login']}'";
+			$query = mysqli_query($con, $sql);
+
+
+			if($query->num_rows != 0) {
+				$row = mysqli_fetch_array($query);
+				if($user_data['Passwd'] == $row['Password']) {
+					header("Location: nauka.php?pytanie=1&zakres_struktury=podstawowy");
+				}
+				else {
+					echo "Hasło niepoprawne!";
+				}
 			}
 			else {
-				document.querySelector("#specjalistyczny").style="background-color: grey !important;";
+				echo "Login nieprawidłowy!";
 			}
 		}
 
-		function add_all_a_to_div() {
-			let holderForA = document.querySelector("#holderForA");
-			let a = document.querySelectorAll("a");
-
-			for(let i=0; i<a.length; i++) {
-				holderForA.appendChild(a[i]);
-			}
-		}
-
-		change_button_color();
-		add_all_a_to_div();
-
-	</script>
-	
+	?>
 
 </body>
 </html>
-
-
-
-
-
-
-
-
---
+++ nauka.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style-nauka.css">
+	<title>Egzaminy na prawo jazdy</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+	<div id="main">
+	<?php
+	
+		include("connection.php");
+
+		$pytanieID = $_GET['pytanie'];
+		$zakres_struktury = $_GET['zakres_struktury'];
+
+		if($zakres_struktury == "podstawowy") {
+			get_data_podstawowe();
+		}
+		else {
+			get_data_specjalistyczne();
+		}
+
+		function get_data_podstawowe() {
+			global $con, $pytanieID;
+
+			//get data from table pytania
+			$sql = "SELECT * FROM pytania_podstawowe WHERE Id=$pytanieID";
+			$query = mysqli_query($con, $sql);
+
+			$data_array = mysqli_fetch_array($query);
+			$next = ($data_array['Id'])+1;
+			$previous = ($data_array['Id']) - 1;
+
+			$media = "data/{$data_array["Media"]}";
+
+			echo
+				"	
+					<div id='questionAndAnswer'>
+						<object id='MediaPlayer' width=320 height=286 classid='CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95' standby='Loading Windows Media Player ...' type='application/x-oleobject' codebase='http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112'> 
+						<param name='filename' value='data/$media'>
+						<param name='Showcontrols' value='True'>
+						<param name='autoStart' value='True'>
+						<param name='wmode' value='transparent'>
+						<embed type='application/x-mplayer2' src='data/$media' name='MediaPlayer' autoStart='True' wmode='transparent' width='320' height='286' ></embed>
+						</object>
+
+						<p>".$data_array['Pytanie']."</p></br>
+						<button class='answer'>Tak</button>
+						<button class='answer'>Nie</button>
+					</div>
+				";
+
+			echo "<div id='navigationMenu'>";
+			if($data_array['Id'] > 1) {
+				echo "<a class='btn btn-danger' href=nauka.php?pytanie=$previous&zakres_struktury=podstawowy>Poprzednie</a> ";
+			}
+
+			echo "<a class='btn btn-success' href=nauka.php?pytanie=$next&zakres_struktury=podstawowy>Następne</a>";
+			echo "</div>";
+			
+		}
+
+		function get_data_specjalistyczne() {
+			global $con, $pytanieID;
+
+			//get data from table pytania
+			$sql = "SELECT * FROM pytania_specjalistyczne WHERE Id=$pytanieID";
+			$query = mysqli_query($con, $sql);
+
+			$data_array = mysqli_fetch_array($query);
+			$next = ($data_array['Id'])+1;
+			$previous = ($data_array['Id']) - 1;
+
+			echo
+				"	
+					<div id='questionAndAnswer'>	
+						<p>".$data_array['Pytanie']."</p></br>
+						<button class='answer'>".$data_array['Odpowiedz_A']."</button>
+						<button class='answer'>".$data_array['Odpowiedz_B']."</button>
+						<button class='answer'>".$data_array['Odpowiedz_C']."</button>
+					</div>
+				";
+			echo "<div id='navigationMenu'>";
+			if($data_array['Id'] > 1) {
+				echo "<a class='btn btn-danger navigation' href=nauka.php?pytanie=$previous&zakres_struktury=specjalistyczny>Poprzednie</a> ";
+			}
+
+			echo "<a class='btn btn-success navigation' href=nauka.php?pytanie=$next&zakres_struktury=specjalistyczny>Następne</a>";
+			echo "</div>";
+			
+		}
+
+
+	?>
+
+		<div id="holderForA">
+			<div id="categoryButtons">
+				<a class="btn btn-primary" id="podstawowy" href=nauka.php?pytanie=1&zakres_struktury=podstawowy>Podstawowy</a>
+				<a class="btn btn-primary" id="specjalistyczny" href=nauka.php?pytanie=1&zakres_struktury=specjalistyczny>Specjalistyczny</a>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<script type="text/javascript">
+		
+		function change_button_color() {
+			let zakres = <?php echo json_encode($zakres_struktury); ?>;
+
+			if(zakres == "podstawowy") {
+				document.querySelector("#podstawowy").style="background-color: grey !important;";
+			}
+			else {
+				document.querySelector("#specjalistyczny").style="background-color: grey !important;";
+			}
+		}
+				
+		change_button_color();
+		
+		//add div into another div
+		let holderForA = document.querySelector("#holderForA");
+		holderForA.appendChild(document.querySelector("#navigationMenu"));
+	
+
+	</script>
+	
+
+</body>
+</html>
+
+
+
+
+
+
+
+
